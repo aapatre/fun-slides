1039056293d3a8ea212e899db3d0c99c4f047ed6
Antariksh Patre <antarikshpatre@gmail.com>
2021-12-01 12:40:54 +0530

    Cleared commented jquery code

--- slider-frontend.js
+++ slider-frontend.js
@@ -1,4 +1,5 @@
 jQuery(document).ready(function(){
+
   jQuery('.fun-slides-frontend-slideshow').slick();
 
   const slideshow_list = jQuery('.fun-slides-frontend-slideshow');
@@ -16,36 +17,5 @@ jQuery(document).ready(function(){
     } )
 
   } )
-
-  // On before slide change
-  jQuery('.fun-slides-frontend-slideshow').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-    // console.log(nextSlide);
-    
-    // console.log(slideshow_list);
-    
-    // Array.from(slideshow_list).forEach((slideshow_obj, index) => {
-      
-    //   console.log(slideshow_obj);
-    //   console.log(index);
-
-    // });
-
-  });
-
-  // setInterval( function() {
-
-  //   console.log(jQuery('div.fun-slides-frontend-slide > video.fun-slides-frontend-video'));
-
-  //   Array.from(jQuery('div.fun-slides-frontend-slide > video.fun-slides-frontend-video')).forEach((slideshow_obj, index) => {
-      
-  //     console.log(slideshow_obj);
-  //     console.log(index);
-
-  //   });
-  
-  // } , 5000);
-
-  
-
 
 });
