01611fdd070e1c8775fe0039add9678780eed1a2
Antariksh Patre <antarikshpatre@gmail.com>
2021-12-02 16:18:08 +0530

    Only the slider with the played video is paused and vice versa

--- slider-frontend.js
+++ slider-frontend.js
@@ -1,23 +1,59 @@
-jQuery(document).ready(function(){
+function getClickedSlideshowObjIndex( clickedObj, classNameOfObjs ) {
 
-  jQuery('.fun-slides-frontend-slideshow').slick();
+  const list_of_objs = jQuery(classNameOfObjs);
+
+  let objIndex;
+
+  Array.from( list_of_objs ).forEach( ( eachObj, index ) => {
+
+    if( jQuery(eachObj).is(clickedObj) ) {
+
+      objIndex = index;
+
+    }
+
+  });
+
+  return objIndex;
+
+}
+
+jQuery(document).ready(function(){
 
   const slideshow_list = jQuery('.fun-slides-frontend-slideshow');
 
-  const all_videos = jQuery('.fun-slides-frontend-video');
+  Array.from( slideshow_list ).forEach( ( each_slideshow, index ) => {
 
-  Array.from(all_videos).forEach( ( video ) => {
+    jQuery(each_slideshow).slick();
 
-    video.addEventListener( 'playing', ( e ) => {
-      
-      slideshow_list.slick( 'slickPause' );
+    const all_videos = jQuery('.fun-slides-frontend-video');
 
-    } );
+    Array.from(all_videos).forEach( ( video ) => {
+  
+      video.addEventListener( 'playing', ( e ) => {
 
-    video.addEventListener( 'pause', ( e ) => {
+        const currentSlideshowObj = jQuery(video).closest('.fun-slides-frontend-slideshow');
 
-      slideshow_list.slick( 'slickPlay' );
+        const currentSlideshowIndex = getClickedSlideshowObjIndex(currentSlideshowObj, '.fun-slides-frontend-slideshow');
 
+        jQuery(slideshow_list[currentSlideshowIndex]).slick( 'slickPause' );
+  
+      } );
+  
+      if( jQuery(each_slideshow).attr('data-sliderautoplay') == 'true' ) {
+
+        video.addEventListener( 'pause', ( e ) => {
+
+          const currentSlideshowObj = jQuery(video).closest('.fun-slides-frontend-slideshow');
+
+          const currentSlideshowIndex = getClickedSlideshowObjIndex(currentSlideshowObj, '.fun-slides-frontend-slideshow');
+  
+          jQuery(slideshow_list[currentSlideshowIndex]).slick( 'slickPlay' );
+    
+        } );
+        
+      }
+  
     } );
 
   } );
